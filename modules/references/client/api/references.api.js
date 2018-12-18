857f147e89754d38e80b802a41e9b439f4a9c8d1
mrkvon <mrkvon@users.noreply.github.com>
2018-12-18 01:12:44 +0100

    api request, list received data

--- modules/references/client/api/references.api.js
+++ modules/references/client/api/references.api.js
@@ -33,7 +33,8 @@ const referenceMapping = [
   ['hostedThem', 'interactions.hostedThem'],
   ['recommend', 'recommend'],
   ['userTo', 'userTo'],
-  ['public', 'public']
+  ['public', 'public'],
+  ['created', 'created']
 ];
 
 /**
@@ -54,7 +55,10 @@ export async function create(reference) {
  * @returns Promise<Reference[]> - array of the found references
  */
 export async function read({ userFrom, userTo }) {
-  const { data: references } = await axios.get(`/api/references?userFrom=${userFrom}&userTo=${userTo}`);
+  const params = {};
+  if (userFrom) params.userFrom = userFrom;
+  if (userTo) params.userTo = userTo;
+  const { data: references } = await axios.get('/api/references', { params });
   return references.map(reference => mapObjectToObject(reference, referenceMapping, true));
 }
 
